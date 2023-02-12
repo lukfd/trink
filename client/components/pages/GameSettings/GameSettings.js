e040cf043c7b98a0bc3b2934b405860b8ca7f641
lukfd <luca.comba98@gmail.com>
2023-02-11 23:37:12 -0600

    Finishing Game Settings.

--- client/components/pages/GameSettings/GameSettings.js
+++ client/components/pages/GameSettings/GameSettings.js
@@ -1,6 +1,6 @@
-import { React, useState } from "react";
-import { Center, VStack, Heading, Button } from "native-base";
-import Players from "../Players/Players"
+import { React, useState } from 'react'
+import { Center, VStack, Heading, Button } from 'native-base'
+import Players from '../Players/Players'
 
 const GameSettings = () => {
   const [isShowTypePage, setIsShowTypePage] = useState(true)
@@ -12,14 +12,32 @@ const GameSettings = () => {
       <Center flex={1}>
         <Heading my={100}>Scegli il tipo di gioco</Heading>
         <VStack space={4} alignItems="center">
-          <Button w="64" h="20" bg="indigo.300" rounded="md" shadow={3} 
-            onPress={() => { 
-              setIsShowTypePage(false); setGameSettings([...gameSettings, {type: 'classic'}])
-            }}>Classico</Button>
-          <Button w="64" h="20" bg="indigo.500" rounded="md" shadow={3} 
-            onPress={() => { 
-              setIsShowTypePage(false); setGameSettings([...gameSettings, {type: 'truthOrDare'}])
-            }}>Obbligo o Verità</Button>
+          <Button
+            w="64"
+            h="20"
+            bg="indigo.300"
+            rounded="md"
+            shadow={3}
+            onPress={() => {
+              setIsShowTypePage(false)
+              setGameSettings([...gameSettings, { type: 'classic' }])
+            }}
+          >
+            Classico
+          </Button>
+          <Button
+            w="64"
+            h="20"
+            bg="indigo.500"
+            rounded="md"
+            shadow={3}
+            onPress={() => {
+              setIsShowTypePage(false)
+              setGameSettings([...gameSettings, { type: 'truthOrDare' }])
+            }}
+          >
+            Obbligo o Verità
+          </Button>
         </VStack>
       </Center>
     )
@@ -30,27 +48,55 @@ const GameSettings = () => {
       <Center flex={1}>
         <Heading my={100}>Scegli la modalità</Heading>
         <VStack space={4} alignItems="center">
-          <Button w="64" h="20" bg="indigo.300" rounded="md" size="lg" shadow={3}
-          onPress={() => { 
-            setIsShowPlayersPage(true); setGameSettings([...gameSettings, {modality: 'easy'}])
-          }}>Easy</Button>
-          <Button w="64" h="20" bg="indigo.500" rounded="md" size="lg" shadow={3}
-          onPress={() => { 
-            setIsShowPlayersPage(true); setGameSettings([...gameSettings, {modality: 'flirt'}])
-          }}>Flirt</Button>
-          <Button w="64" h="20" bg="indigo.700" rounded="md" size="lg" shadow={3}
-          onPress={() => { 
-            setIsShowPlayersPage(true); setGameSettings([...gameSettings, {modality: 'hot'}])
-          }}>Hot</Button>
+          <Button
+            w="64"
+            h="20"
+            bg="indigo.300"
+            rounded="md"
+            size="lg"
+            shadow={3}
+            onPress={() => {
+              setIsShowPlayersPage(true)
+              setGameSettings([...gameSettings, { modality: 'easy' }])
+            }}
+          >
+            Easy
+          </Button>
+          <Button
+            w="64"
+            h="20"
+            bg="indigo.500"
+            rounded="md"
+            size="lg"
+            shadow={3}
+            onPress={() => {
+              setIsShowPlayersPage(true)
+              setGameSettings([...gameSettings, { modality: 'flirt' }])
+            }}
+          >
+            Flirt
+          </Button>
+          <Button
+            w="64"
+            h="20"
+            bg="indigo.700"
+            rounded="md"
+            size="lg"
+            shadow={3}
+            onPress={() => {
+              setIsShowPlayersPage(true)
+              setGameSettings([...gameSettings, { modality: 'hot' }])
+            }}
+          >
+            Hot
+          </Button>
         </VStack>
       </Center>
     )
   }
 
   function playersPage() {
-    console.log('HERE')
-    console.log(gameSettings)
-    return (<Players gameSettings={gameSettings}/>)
+    return <Players gameSettings={gameSettings} />
   }
 
   if (isShowTypePage) {
@@ -59,7 +105,7 @@ const GameSettings = () => {
     return playersPage()
   } else {
     return modalityPage()
-  } 
+  }
 }
 
-export default GameSettings;
+export default GameSettings
